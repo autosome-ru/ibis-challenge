f0ccb294b1c5abe9daeb6cca89e337ec86dcd327
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2024-03-05 21:53:51 +0000

    modify parse_grecobit.sh

--- cli/parse_grecobit.sh
+++ cli/parse_grecobit.sh
@@ -1,9 +1,12 @@
-#python parse_grecobit_chipseq.py\
-#       --genome /home_local/dpenzar/hg38\
-#       --black_list_regions /home_local/dpenzar/ibis-challenge/benchmark/data/blacklist.bed\
-#       --valid_hide_regions /home_local/dpenzar/centromers_split/chs_hide.bed
-#python parse_grecobit_affiseq.py\
-#       --genome /home_local/dpenzar/hg38\
-#       --black_list_regions /home_local/dpenzar/ibis-challenge/benchmark/data/blacklist.bed\
-#       --valid_hide_regions /home_local/dpenzar/centromers_split/ghts_hide.bed
+python parse_grecobit_chipseq.py\
+       --genome /home_local/dpenzar/hg38\
+       --black_list_regions /home_local/dpenzar/ibis-challenge/benchmark/data/blacklist.bed\
+       --valid_hide_regions /home_local/dpenzar/centromers_split/chs_hide.bed
+python parse_grecobit_affiseq.py\
+       --genome /home_local/dpenzar/hg38\
+       --black_list_regions /home_local/dpenzar/ibis-challenge/benchmark/data/blacklist.bed\
+       --valid_hide_regions /home_local/dpenzar/centromers_split/ghts_hide.bed
 python parse_grecobit_pbm.py --neg2pos_ratio 10
+python parse_grecobit_smileseq.py 
+python preprocess_smileseq.py
+python parse_grecobit_htselex.py
