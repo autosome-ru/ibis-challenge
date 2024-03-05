3a500448f58b74442eaf8c402467686f7cb04e72
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2024-03-05 21:02:18 +0000

    add logging

--- cli/parse_grecobit_chipseq.py
+++ cli/parse_grecobit_chipseq.py
@@ -5,67 +5,10 @@ import glob
 
 from pathlib import Path
 import sys 
+import parse
 
-
-sys.path.append("/home_local/dpenzar/bibis_git/ibis-challenge")
-
-from bibis.chipseq.peakfile import ChIPPeakList
-
-from bibis.chipseq.config import ChipSeqConfig, ChipSeqSplit, ForeignConfig, GenomeSampleConfig, ShadesConfig
-from bibis.ibis_utils import ibis_default_name_parser
-
-def extract_files(dir, row, parser=ibis_default_name_parser()):
-    files = glob.glob(str(dir / f"{row.tf}.*"))
-    if len(files) == 0:
-        raise Exception(f"No files found for tf: {row.tf}")
-    info = {}
-    for fl in files:
-        if res := parser.parse(Path(fl).name):
-            name = res["name"] #type: ignore
-            if name not in row.replics:
-                print(f"Skipping replic {name} for {row.tf} as it wasn't specified in the table", file=sys.stderr)
-                continue
-            info[name] = fl
-        else:
-            raise Exception("Wrong naming former")
-    if len(info) != len(row.replics):
-        raise Exception(f"No files found for replics: {set(row.replics) - set(info)}")
-    return info 
-
-def merge_files(train_files, valid_files, dest_dir):
-    merged_info = {}
-    for name, train_fl in train_files.items():
-        val_fl = valid_files[name]
-          
-        dest_path = dest_dir / f"{name}.peaks"
-        with open(dest_path, "w") as out:
-            with open(train_fl, 'r') as inp:
-                for line in inp:
-                    print(line, file=out, end="")
-            with open(val_fl, "r") as inp:
-                for line in inp:
-                    print(line, file=out, end="")
-        merged_info[name] = str(dest_path)
-    return merged_info
-
-def process_row(row, train_dir, valid_dir, out_dir):
-    train_files = extract_files(train_dir, row)
-    valid_files = extract_files(valid_dir, row)
-    dest_dir = out_dir / row.stage / row.tf
-    dest_dir.mkdir(exist_ok=True, parents=True)
-    replics_info = merge_files(train_files=train_files,
-                valid_files=valid_files,
-                dest_dir=dest_dir) 
-    
-    return replics_info
-    
-
-LEADERBOARD_EXCEL = "/home_local/dpenzar/IBIS TF Selection - Nov 2022 _ Feb 2023.xlsx"
-SPLIT_SHEET_NAME = "v3 TrainTest marked (2023)"
-ILYA_DIR = Path("/home_local/vorontsovie/greco-data/release_8d.2022-07-31/full/CHS/")
-TRAIN_INT = ILYA_DIR / "Train_intervals"
-VAL_INT = ILYA_DIR / "Val_intervals"
-OUT_DIR = Path("/home_local/dpenzar/BENCH_FULL_DATA/CHS")
+def ibis_default_name_parser():
+    return parse.compile("{tf_name}.{fl}@{exp_type}@{name}@Peaks.{unique_tag}.{dt_type}.peaks")
 
 
 parser = argparse.ArgumentParser()
@@ -116,19 +59,100 @@ parser.add_argument("--seed",
 parser.add_argument("--n_procs",
                     type=int,
                     default=1)
-
+parser.add_argument("--bibis_root",
+                    default="/home_local/dpenzar/bibis_git/ibis-challenge",
+                    type=str)
+parser.add_argument("--log_path",
+                    default='log.txt')
+parser.add_argument("--logger_name",
+                    default="parse_chipseq")
 
 args = parser.parse_args()
-print(args)
-    
+sys.path.append(args.bibis_root) 
 
+
+from bibis.peaks.peakfile import PeakList
+
+from bibis.peaks.config import PeakSeqConfig, PeakSeqSplit, ForeignConfig, GenomeSampleConfig, ShadesConfig
+
+from bibis.logging import get_logger, BIBIS_LOGGER_CFG
+BIBIS_LOGGER_CFG.set_path(path=args.log_path)
+logger = get_logger(name=args.logger_name, path=args.log_path)
+
+
+def log_splits(cfg: PeakSeqConfig, splits: list[str]=None):
+    if splits is None:
+        splits = ['train', 'test']
+
+    for split in splits:
+        split_inst = cfg.splits.get(split)
+        if split_inst is None:
+            logger.info(f"For factor {cfg.tf_name} no replics are going to {split}")
+        else:
+            reps = ", ".join(split_inst.reps.keys())
+            logger.info(f"For factor {cfg.tf_name} the following replics are going to {split}: {reps}")    
+
+
+def extract_files(dir, row, parser=ibis_default_name_parser()):
+    files = glob.glob(str(dir / f"{row.tf}.*"))
+    if len(files) == 0:
+        raise Exception(f"No files found for tf: {row.tf}")
+    info = {}
+    for fl in files:
+        if res := parser.parse(Path(fl).name):
+            name = res["name"] #type: ignore
+            if name not in row.replics:
+                print(f"Skipping replic {name} for {row.tf} as it wasn't specified in the table", file=sys.stderr)
+                continue
+            info[name] = fl
+        else:
+            raise Exception("Wrong naming former")
+    if len(info) != len(row.replics):
+        raise Exception(f"No files found for replics: {set(row.replics) - set(info)}")
+    return info 
+
+def merge_files(train_files, valid_files, dest_dir):
+    merged_info = {}
+    for name, train_fl in train_files.items():
+        val_fl = valid_files[name]
+          
+        dest_path = dest_dir / f"{name}.peaks"
+        with open(dest_path, "w") as out:
+            with open(train_fl, 'r') as inp:
+                for line in inp:
+                    print(line, file=out, end="")
+            with open(val_fl, "r") as inp:
+                for line in inp:
+                    print(line, file=out, end="")
+        merged_info[name] = str(dest_path)
+    return merged_info
+
+def process_row(row, train_dir, valid_dir, out_dir):
+    train_files = extract_files(train_dir, row)
+    valid_files = extract_files(valid_dir, row)
+    dest_dir = out_dir / row.stage / row.tf
+    dest_dir.mkdir(exist_ok=True, parents=True)
+    replics_info = merge_files(train_files=train_files,
+                valid_files=valid_files,
+                dest_dir=dest_dir) 
+    
+    return replics_info
+    
+LEADERBOARD_EXCEL = "/home_local/dpenzar/IBIS TF Selection - Nov 2022 _ Feb 2023.xlsx"
+SPLIT_SHEET_NAME = "v3 TrainTest marked (2023)"
+ILYA_DIR = Path("/home_local/vorontsovie/greco-data/release_8d.2022-07-31/full/CHS/")
+TRAIN_INT = ILYA_DIR / "Train_intervals"
+VAL_INT = ILYA_DIR / "Val_intervals"
+OUT_DIR = Path("/home_local/dpenzar/BENCH_FULL_DATA/CHS")
+
+logger.info("Reading ibis metainfo for ChIPSeq")
 ibis_table = pd.read_excel(LEADERBOARD_EXCEL, sheet_name=SPLIT_SHEET_NAME)
 ibis_table = ibis_table[['Transcription factor', 'CHS', 'ChIP-Seq', 'Stage']]
 ibis_table.columns = ['tf', 'replics', 'split', 'stage']
 ibis_table['replics'] = ibis_table['replics'].str.split(',')
 
-
-for stage in ('Final', 'Leaderboard'):
+for stage in ('Leaderboard', 'Final'):
+    logger.info(f"Spliting {stage} datasets and writing configs")
     stage_table = ibis_table[ibis_table.stage == stage]
     stage_info = {}
     for ind, row in stage_table.iterrows():
@@ -137,54 +161,48 @@ for stage in ('Final', 'Leaderboard'):
                     valid_dir=VAL_INT, 
                     out_dir=OUT_DIR / 'data')
         stage_info[row.tf] = tf_info
-    
-    
-        
+     
     configs_dir = OUT_DIR / "configs" / stage
     configs_dir.mkdir(exist_ok=True, parents=True)
     
-    
     test_files = {}
-    to_save: dict[str,  tuple[Path, ChipSeqConfig]] = {}
+    to_save: dict[str,  tuple[Path, PeakSeqConfig]] = {}
     
     assert len(stage_table) == len(set(row.tf for _, row in stage_table.iterrows()))
     
     for ind, row in stage_table.iterrows():
         tf = row.tf
-        replics = stage_info[tf]        
-        paths = list(replics.values())
-        
-        tf_peaks = [ChIPPeakList.read(t) for t in paths]
+        replics = stage_info[tf]
+        rep_names = list(replics.keys())                
+        tf_peaks = [PeakList.read(t) for t in replics.values()]
         tf_beds = [f.to_beddata() for f in tf_peaks]
             
         if row.split == "Train":
-            splits = {"train": ChipSeqSplit(paths=paths,
+            splits = {"train": PeakSeqSplit(reps=replics,
                                             chroms=args.train_chroms,
                                             hide_regions=None)}
         elif row.split == "Test":
             ind, _ = max(enumerate(tf_beds), key=lambda x: len(x[1]))
-            test_peak_path = paths.pop(ind)
-            test_files[tf] = test_peak_path
-            splits = {"test": ChipSeqSplit(paths=[test_peak_path],
+            test_rep = rep_names.pop(ind)
+            test_files[tf] = replics[test_rep]
+            splits = {"test": PeakSeqSplit(reps={test_rep: replics[test_rep]},
                                            chroms=args.valid_chroms,
                                            hide_regions=args.valid_hide_regions)}
-           
-            
         elif row.split == "Train/Test":
             ind, _ = max(enumerate(tf_beds), key=lambda x: len(x[1]))
-            test_peak_path = paths.pop(ind)
-            train_peaks_paths = paths
-            test_files[tf] = test_peak_path
-            splits = {"train": ChipSeqSplit(paths=train_peaks_paths,
+            test_rep = rep_names.pop(ind)
+            test_files[tf] = replics[test_rep]
+            train_replics = {rep: replics[rep] for rep in rep_names}
+            splits = {"train": PeakSeqSplit(reps=train_replics,
                                             chroms=args.train_chroms,
                                             hide_regions=None),
-                      "test": ChipSeqSplit(paths=[test_peak_path],
+                      "test": PeakSeqSplit(reps={test_rep: replics[test_rep]},
                                            chroms=args.valid_chroms,
                                            hide_regions=args.valid_hide_regions)}
         else:
             raise Exception("Wrong split: {row.split}")
         
-        config = ChipSeqConfig(tf_name=tf,
+        config = PeakSeqConfig(tf_name=tf,
                                splits=splits,
                                black_list_path=args.black_list_regions,
                                friends_path=[],
@@ -200,6 +218,7 @@ for stage in ('Final', 'Leaderboard'):
                                                                         n_procs=args.n_procs,
                                                                         exact=args.exact_genome,
                                                                         precalc_profile=False))
+        log_splits(config)
         path = configs_dir / f"{tf}.json"
         to_save[tf] = (path, config)
         
