52e656cab48e18206af21841763c4ccc7f6c5240
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2024-03-05 13:27:10 +0000

    add logging

---
+++ bibis/logging.py
@@ -0,0 +1,39 @@
+import logging
+
+NO_FILE_LOGGER = "__NO__"
+BIBIS_LOGGER_NAME = "bibis"
+BIBIS_LOGGING_PATH = "bibis.log"
+
+def get_logger(name: str, path: str | None = None) -> logging.Logger:
+    if path is None:
+        path = f"{name}.log"
+
+    logger = logging.getLogger(name)
+    
+    stream_handler = logging.StreamHandler()
+    stream_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(stream_format)
+    logger.addHandler(stream_handler)
+
+    if path != NO_FILE_LOGGER:
+        file_handler = logging.FileHandler(path)
+        file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_format)
+        logger.addHandler(file_handler)
+
+def get_bibis_logger() -> logging.Logger:
+    return get_logger(name=BIBIS_LOGGER_NAME, path=BIBIS_LOGGER_NAME)
+
+
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
+logger.warning('This is a warning')
+logger.error('This is an error')
