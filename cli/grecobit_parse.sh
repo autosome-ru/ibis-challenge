9fbbd0ab429c71bcdf3e8ad45821e86742646a57
dmitrypenzar1996 <dmitry_penzar_1996@mail.ru>
2024-03-06 23:47:01 +0000

    change blacklist files for chs and ghts

--- cli/grecobit_parse.sh
+++ cli/grecobit_parse.sh
@@ -2,13 +2,13 @@ OUT_DIR="/home_local/dpenzar/BENCHMARK/BENCHMARK_DATA"
 python parse_grecobit_pbm.py --neg2pos_ratio 10\
        --out_dir ${OUT_DIR}/PBM
 python parse_grecobit_chipseq.py\
-      --genome /home_local/dpenzar/hg38\
-       --black_list_regions /home_local/dpenzar/ibis-challenge/benchmark/data/blacklist.bed\
+      --genome /home_local/dpenzar/final_genome/hg38/\
+       --black_list_regions /home_local/dpenzar/bibis_git/ibis-challenge/data/Ns_ENCODE_blacklist.bed\
        --valid_hide_regions /home_local/dpenzar/bibis_git/ibis-challenge/data/centromers_split/ghts_hide.bed\
        --out_dir ${OUT_DIR}/CHS/
 python parse_grecobit_affiseq.py\
-       --genome /home_local/dpenzar/hg38\
-       --black_list_regions /home_local/dpenzar/ibis-challenge/benchmark/data/blacklist.bed\
+       --genome /home_local/dpenzar/final_genome/hg38/\
+       --black_list_regions  /home_local/dpenzar/bibis_git/ibis-challenge/data/Ns_ENCODE_blacklist.bed\
        --valid_hide_regions /home_local/dpenzar/bibis_git/ibis-challenge/data/centromers_split/ghts_hide.bed\
        --out_dir ${OUT_DIR}/GHTS
 python parse_grecobit_smileseq.py --out_dir ${OUT_DIR}/SMS/RAW
